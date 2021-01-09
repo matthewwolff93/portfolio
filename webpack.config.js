1e87df76391bf6b179de5d060979161be17e02ac
Matthew Wolff <matthew.wolff93@gmail.com>
2021-01-09 13:26:11 -0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({template: 'src/index.html'})
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    hot: true,
+    port: 9000
+  }
+}
